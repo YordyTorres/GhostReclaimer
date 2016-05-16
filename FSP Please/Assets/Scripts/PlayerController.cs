43b714b4b76d22be01ea6a742e93679d9043f600
YordyTorres <yordy.ytc.torres@Gmail.com>
2016-05-16 15:00:27 -0400

    Add files via upload

---
+++ FSP Please/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//so that when added, it adds PlayerMotor & RigidBody//
+[RequireComponent(typeof(PlayerMotor))]
+
+public class PlayerController : MonoBehaviour {
+
+//serialize field keeps it public but protected
+	[SerializeField]
+	private float speed = 5f; //speed of movement//
+	[SerializeField]
+	private float lookSensitivity = 3f; //mouse look around sensitivity//
+
+	[SerializeField]
+	private float thForce = 1000f; //thruster force. for flying//
+
+	private PlayerMotor motor;  //reference to PlayerMotor//
+
+	void Start ()
+	{
+		motor = GetComponent<PlayerMotor> ();
+	}
+
+	void Update ()
+	{
+		//Calculate movement velocity as a 3D vector//
+		float xMov = Input.GetAxisRaw("Horizontal"); //to perform movement on x axis//
+		float zMov = Input.GetAxisRaw("Vertical");  //to perform movement on y axis//
+
+		//calculation for which direction the player is moving in each respective axis with (0,0,0) being no movement at all//
+		Vector3 movHor = transform.right * xMov;  //to perform movement on x axis//
+		Vector3 movVer = transform.forward * zMov; //to perform movement on y axis//
+
+
+		//final movement vector//
+		Vector3 _velocity = (movHor + movVer).normalized * speed;
+
+		//apply movement//
+		motor.Move(_velocity);
+
+		//calculate rotation as a 3d vector. For turning around.//
+		float yRot = Input.GetAxisRaw("Mouse X"); //for turning left and right//
+
+		Vector3 _rotation = new Vector3 (0f, yRot, 0f) * lookSensitivity;
+
+		//apply rotation//
+		motor.Rotate(_rotation);
+
+		//calculate camera rotation as a 3d vector. For turning around.//
+		float xRot = Input.GetAxisRaw("Mouse Y"); //for turning left and right//
+
+		Vector3 _cameraRotation = new Vector3 (xRot, 0f, 0f) * lookSensitivity;
+
+		Vector3 _thForce = Vector3.zero;
+		//apply camera rotation//
+		motor.RotateCamera(_cameraRotation);
+
+		//Calculate the thForce based on player input// 
+		if (Input.GetButton ("Jump")) 
+		{
+			_thForce = Vector3.up * thForce;
+		}
+
+		// Apply thruster force//
+		motor.ApplyThruster (_thForce);
+
+	}
+
+}
---
+++ FSP Please/Assets/Scripts/PlayerMotor.cs
@@ -0,0 +1,81 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//to always have rigidbody with the player motor//
+[RequireComponent(typeof(Rigidbody))]
+
+public class PlayerMotor : MonoBehaviour {
+
+	[SerializeField]
+	private Camera cam;
+
+	private Vector3 velocity = Vector3.zero; //variable for velocity//
+	private Vector3 rotation = Vector3.zero; //variable for rotation//
+	private Vector3 cameraRotation = Vector3.zero; //variable for camera rotation//
+	private Vector3 thForce = Vector3.zero; 
+	private Rigidbody rb;
+
+	void Start ()
+	{
+		rb = GetComponent<Rigidbody> ();
+	}
+	 
+	//Gets a movement vector//
+	public void Move (Vector3 _velocity)
+	{
+		velocity = _velocity;	
+	}
+
+
+	//Gets a rotational vector//
+	public void Rotate (Vector3 _rotation)
+	{
+		rotation = _rotation;	
+	}
+
+	//Gets a rotational vector for the camera//
+	public void RotateCamera (Vector3 _cameraRotation)
+	{
+		cameraRotation = _cameraRotation;	
+	}
+
+	// Get a force vector for our thruster//
+	public void ApplyThruster (Vector3 _thForce)
+	{
+		thForce = _thForce;
+	}
+
+	//Run every physics iteration//
+	void FixedUpdate ()
+	{
+		PerformMovement ();
+		PerformRotation ();
+	}
+
+	//perform movement based on velocity variable//
+	void PerformMovement()
+	{
+		if (velocity != Vector3.zero) 
+		{
+			rb.MovePosition (rb.position + velocity * Time.fixedDeltaTime);
+		}
+
+		if (thForce != Vector3.zero) 
+		{
+			rb.AddForce (thForce * Time.fixedDeltaTime, ForceMode.Acceleration);
+		}
+
+	}
+
+	// perform rotation//
+	void PerformRotation ()
+	{
+		rb.MoveRotation(rb.rotation * Quaternion.Euler(rotation));
+		if (cam != null) 
+		{
+			cam.transform.Rotate (-cameraRotation);
+		}
+	}
+
+
+}
